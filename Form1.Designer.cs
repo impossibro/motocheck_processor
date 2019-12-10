ab8021bbe68d9dcbb8676dc05d751647050dc55e
Kavinesh Kumar <kavinesh2@gmail.com>
2019-12-11 11:13:42 +1300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Motochek_Processor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(132, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(132, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Permit Data File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select RUC Data File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Motochek Files (*.mc.txt)|*.mc.txt|All files (*.*)|*.*";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "Motochek Files (*.mc.txt)|*.mc.txt|All files (*.*)|*.*";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Process";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 117);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Motocheck Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Motochek_Processor
+{
+    public partial class Form1 : Form
+    {
+        private Boolean file1OK = false;
+        private Boolean file2OK = false;
+
+        private String[] rawPermitData;
+        private String[] rawRUCData;
+
+        private String rego;
+        private String vin;
+        private String regExp;
+        private String wofExp;
+        private String rucExp;
+
+        private List<String> outputs;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetVars();
+            outputs = new List<String>();
+            outputs.Add("rego,vin,regExp,wofExp,rucExp");
+        }
+        
+        //==========FILE SELECTION==========//
+
+        //hidden variables to make sure file path fields are not empty.
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                file1OK = true;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog2.FileName;
+                file2OK = true;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //check if both files have been selected and then process them.
+            if (file1OK && file2OK)
+            {
+                rawPermitData = File.ReadAllLines(textBox1.Text);
+                rawRUCData = File.ReadAllLines(textBox2.Text);
+                ProcessRegWOFFile();
+            }
+            else
+            {
+                MessageBox.Show("Please select both files", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void ProcessRegWOFFile()
+        {
+            for(int i = 0; i < rawPermitData.Length; i++)
+            {
+                if (i == 0)
+                {
+                    //ignore first line of the text file.
+                    continue;
+                }
+                else
+                {
+                    rego = rawPermitData[i].Substring(1, 6);
+                    vin = rawPermitData[i].Substring(13, 17);
+                    regExp = rawPermitData[i].Substring(627, 2) + "-" + rawPermitData[i].Substring(629, 2) + "-" + rawPermitData[i].Substring(631, 4);
+                    
+                    if(rawPermitData[i].Substring(687, 1) == "Y")
+                    {
+                        wofExp = rawPermitData[i].Substring(697, 2) + "-" + rawPermitData[i].Substring(699, 2) + "-" + rawPermitData[i].Substring(701, 4);
+                    }
+                    else
+                    {
+                        wofExp = rawPermitData[i].Substring(679, 2) + "-" + rawPermitData[i].Substring(681, 2) + "-" + rawPermitData[i].Substring(683, 4);
+                    }
+
+                    if(rawPermitData[i].Substring(705, 1) == "Y")
+                    {
+                        GetRUCExpiry(rego);
+                    }
+
+                    outputs.Add(rego + "," + vin + "," + regExp + "," + wofExp + "," + rucExp);                    
+                    //MessageBox.Show("Rego: " + rego + "\nVIN: " + vin + "\nReg Expiry: " + regExp + "\nWOF Expiry: " + wofExp + "\nRUC Expiry: " + rucExp, "Motochek Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ResetVars();
+                }
+            }
+            File.WriteAllLines("MotoCheckOutput.csv", outputs);
+            MessageBox.Show("Motocheck file processing complete!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void GetRUCExpiry(String reg)
+        {
+            Boolean gotRUC = false;
+
+            for(int i = 0; i < rawRUCData.Length; i++)
+            {
+                if(i == 0)
+                {
+                    //ignore first line of the text file
+                    continue;
+                }
+                else
+                {
+                    if(rawRUCData[i].Substring(1, 6) == reg)
+                    {
+                        rucExp = rawRUCData[i].Substring(128, 8);
+                        gotRUC = true;
+                    }
+                }
+                if(gotRUC)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void ResetVars()
+        {
+            this.rego = "";
+            this.vin = "";
+            this.regExp = "";
+            this.wofExp = "";
+            this.rucExp = "0";
+        }
+
+
+    }
+}
