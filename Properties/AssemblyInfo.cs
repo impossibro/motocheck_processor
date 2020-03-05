d1701b3b54ec867e2e4186529f63eda75b6d24e8
Kavinesh Kumar <kavinesh2@gmail.com>
2020-03-06 12:24:47 +1300

    updated assembly info.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cartrack New Zealand Ltd.")]
 [assembly: AssemblyProduct("Motochek Processor")]
-[assembly: AssemblyCopyright("Copyright Cartrack New Zealand Ltd. ©  2019")]
+[assembly: AssemblyCopyright("Copyright Kavinesh Kumar ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
